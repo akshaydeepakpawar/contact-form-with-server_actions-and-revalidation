a25c34681bd62bcaf067decb689f5cec67a0c1c9
akshay pawar <ap3690700@gmail.com>
2026-03-24 20:13:39 +0530

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
-import dbConnect from "@/lib/db"
+import dbConnect from "@/lib/db";
+import ContactForm from "@/components/contact-form"
 
 export default async function Home() {
   await dbConnect();
   return (
-   <div>
-    <h1>hello world</h1>
-   </div>
+    <main className="min-h-screen py-12 px-4">
+      <div className="container mx-auto">
+        <div className="text-center mb-12">
+          <h1 className="text-4xl font-bold mb-4">Server actions Demo</h1>
+          <p className="text-xl text-gray-600 max-2xl mx-auto">Contact form with MongoDB and revalidation</p>
+        </div>
+        <ContactForm/>
+      </div>
+    </main>
   );
 }
